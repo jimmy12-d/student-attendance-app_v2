6b2d8691c25f34daeaf8f383dd68f3fab984da11
Your Name <kemhoutlem@gmail.com>
2025-06-26 18:45:04 +0700

    working pwa

--- .firebase/rodwell-attendance/functions/next.config.js
+++ .firebase/rodwell-attendance/functions/next.config.js
@@ -1,19 +1,17 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
+// next.config.js
+var withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development"
 });
-
-const nextConfig = withPWA({
+var nextConfig = withPWA({
   // Your regular Next.js config options here
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+    ignoreDuringBuilds: true
+  }
 });
-
-module.exports = nextConfig; 
+module.exports = nextConfig;
